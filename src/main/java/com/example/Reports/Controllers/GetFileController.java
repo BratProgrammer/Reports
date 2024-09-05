7656ac89726fe60b32961424c01580ab19941eae
BratProgrammer <travckin.4@yandex.ru>
2024-09-05 19:26:18 +0300

    Убран ненужный secret из application.properties

--- src/main/java/com/example/Reports/Controllers/GetFileController.java
+++ src/main/java/com/example/Reports/Controllers/GetFileController.java
@@ -5,10 +5,12 @@ import com.example.Reports.Services.FileGeneratorKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/v1/reports_getter")
